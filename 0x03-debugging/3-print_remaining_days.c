83d4d4959aa9052fb343be44bb7a8d51d9b78cfc
PRIDENDLOVU <pridendlovu.p@gmail.com>
2023-06-21 16:11:43 -0700

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,5 +1,3 @@
-#include "main.h"
-
 /**
  * largest_number - returns the largest of 3 numbers
  * @a: first integer
@@ -31,3 +29,4 @@ int largest_number(int a, int b, int c)
 
 return (largest);
 }
+
