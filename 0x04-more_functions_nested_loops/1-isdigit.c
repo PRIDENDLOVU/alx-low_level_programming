11391a75533dbba20f9508f6343466b615ed6900
PRIDENDLOVU <pridendlovu.p@gmail.com>
2023-06-22 12:23:35 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,17 +1,17 @@
 #include "main.h"
-
 /**
- * _isdigit - check if numbers are 0 - 9
- * @c: char to be checked
- *
- * Return: 0 or 1
- */
-
-int _isdigit(int c)
+* _isupper - checks for uppercase character
+* @c: Variable text
+* Return: Always 0.
+*/
+int _isupper(int c)
 {
-        if (c >= '0' && c <= '9')
-		return (1);
-	else
 
-		return (0);
+ if (c >= 'A' && c <= 'Z')
+ {
+ return (1);
+ }
+ else
+{
+ return (0);
 }
