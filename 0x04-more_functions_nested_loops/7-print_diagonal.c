c38b3d95fdcc729d1df3e1ea92a54370050912bf
PRIDENDLOVU <pridendlovu.p@gmail.com>
2023-06-22 14:11:06 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -2,18 +2,13 @@
 
 
 /**
-
  * print_diagonal - draws a diagonal line on the terminal
-
  * @n: number of times the character \ should be printed
-
  */
-
 
 void print_diagonal(int n)
 
 {
-
         if (n <= 0)
 
         {
@@ -52,6 +47,3 @@ void print_diagonal(int n)
         }
 
 }
-
-
-
