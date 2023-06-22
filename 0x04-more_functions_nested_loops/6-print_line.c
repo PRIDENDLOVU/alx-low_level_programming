147acd907197c8e4d4b6a5e696e8dc653f778229
PRIDENDLOVU <pridendlovu.p@gmail.com>
2023-06-22 13:27:09 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,11 +3,11 @@
 
 /**
 
- * print_line - draws a straight line in the terminal
+* print_line - draws a straight line in the terminal
 
- * @n: number of times the character _ should be printed
+* @n: number of times the character _ should be printed
 
- */
+*/
 
 void print_line(int n)
 
