826e2138adf180ef1dff59ff84994ab162591449
PRIDENDLOVU <pridendlovu.p@gmail.com>
2023-06-22 11:20:35 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-
 /**
 
  * _isupper - uppercase letters
@@ -8,22 +7,18 @@
  * @c: char to check
 
  *
-
  * Return: 0 or 1
 
  */
 
-
 int _isupper(int c)
 
 {
-
         if (c >= 'A' && c <= 'Z')
 
                 return (1);
 
         else
 
-		return (0);
-
+                return (0);
 }
