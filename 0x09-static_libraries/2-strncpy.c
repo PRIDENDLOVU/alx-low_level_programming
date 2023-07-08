4a65bda6035f1a264464f0234aa2e803db6fc5e9
PRIDENDLOVU <pridendlovu.p@gmail.com>
2023-07-08 07:23:35 -0700

    libmy.a

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -1,11 +1,10 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
-/**
- * _islower - checks for lowercase character
- * @c: the character to check
- * Return: 1 if c is lowercase, 0 otherwise
- */
-int _islower(int c)
+char *_strncpy(char *dest, char *src, int n)
 {
-	return (c >= 'a' && c <= 'z');
+    return strncpy(dest, src, n);
 }
