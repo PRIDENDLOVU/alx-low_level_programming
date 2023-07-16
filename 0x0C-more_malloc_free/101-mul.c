eab67587a8994b8cea6fc5e643a36fec083313e8
PRIDENDLOVU <pridendlovu.p@gmail.com>
2023-07-16 02:56:04 +0200

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -76,7 +76,7 @@ exit(98);
 int main(int argc, char *argv[])
 {
 char *s1, *s2;
-int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+int len1, len2, len, i, carry,*result, a = 0;
 
 s1 = argv[1], s2 = argv[2];
 if (argc != 3 || !is_digit(s1) || !is_digit(s2))
@@ -92,18 +92,21 @@ result[i] = 0;
 for (len1 = len1 - 1; len1 >= 0; len1--)
 {
 digit1 = s1[len1] - '0';
-carry = 0
-for (len2 = _strlen(s2) - 1; len2 >= 0
-{
+carry = 0;for (len2 = _strlen(s2) - 1; len2 >= 0;
+
+ {
 digit2 = s2[len2] - '0';
 carry += result[len1 + len2 + 1] + (digit1 *digit2);
 result[len1 + len2 + 1] = carry % 10;
 carry /= 10;
+
 }
+
 if (carry > 0)
 result[len1 + len2 + 1] += carry;
-}
+ }
 for (i = 0; i < len - 1; i++)
+
 {
 if (result[i])
 a = 1;
@@ -114,6 +117,6 @@ if (!a)
 
 _putchar('0');
 _putchar('\n');
-free(result)
+free(result);
 return (0);
 }
